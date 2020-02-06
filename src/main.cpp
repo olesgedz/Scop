c119cab3c3cff6d7be56963e5a194a53aa35257e
olesgedz <gedzoles@gmail.com>
2020-02-07 01:40:58 +0300

    round camera

--- src/main.cpp
+++ src/main.cpp
@@ -182,6 +182,14 @@ int main()
 	ourShader.setInt("texture2", 1);
 	glEnable(GL_DEPTH_TEST);
 
+	// // Camera
+	// glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+	// glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+	// glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+	// glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	// glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+	// glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -207,7 +215,10 @@ int main()
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
 		model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		// retrieve the matrix uniform locations
 		unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
