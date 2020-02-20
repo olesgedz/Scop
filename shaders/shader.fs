eabf95e2d3917fb559d47c5bb7c948fbef03da56
olesgedz <olesgedz@rambler.ru>
2020-02-20 19:57:37 +0300

    Phong global

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -1,25 +1,34 @@
+// Fragment shader:
+// ================
 #version 330 core
-
 out vec4 FragColor;
-in  vec3 Normal;
-in  vec3 FragPos;
-uniform vec3 objectColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec3 LightPos;   // extra in variable, since we need the light position in view space we calculate this in the vertex shader
+
 uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+uniform vec3 objectColor;
 
 void main()
 {
-    float specularStrength = 0.5;
+    // ambient
     float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(normalize(Normal), lightDir), 0.0);
+    vec3 ambient = ambientStrength * lightColor;    
+    
+     // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(LightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, normalize(Normal));
+    
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(-FragPos); // the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
+    vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
+    vec3 specular = specularStrength * spec * lightColor; 
+    
     vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
 }
